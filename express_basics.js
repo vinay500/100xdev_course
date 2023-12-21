4e93c0cdd3cdffb5f313493a819d6219067eff91
vinay500 <vinaymadugula20@gmail.com>
2023-12-21 22:54:42 +0530

    adding hospital.js

--- express_basics.js
+++ express_basics.js
@@ -1,16 +1,16 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.get('/',function(request,response){
-    // getting request/url and request/url parameters
-    a_param = request.query.a
-    b_param = request.query.b
-    sum = a_param + b_param
-    // sending response for get request
-    response.send(`in express app, sum = ${sum}`);
-})
+// app.get('/',function(request,response){
+//     // getting request/url and request/url parameters
+//     a_param = parseInt(request.query.a);
+//     b_param = parseInt(request.query.b);
+//     sum = a_param + b_param;
+//     // sending response for get request
+//     response.send(`in express app, sum = ${sum}`);
+// })
 
-// listening on a port
-// 'listening on port 3000' will be logged in console when this express is up and running
-app.listen(3000, console.log(`listening on port 3000`));
+// // listening on a port
+// // 'listening on port 3000' will be logged in console when this express is up and running
+// app.listen(3000, console.log(`listening on port 3000`));
